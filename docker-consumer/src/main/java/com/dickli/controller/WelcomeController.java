8d46d695d20f4fbcdebc23d325268fd4f3b7fa24
dickli1986 <406389474@qq.com>
2022-09-21 19:21:29 +0800

    docker

--- docker-consumer/src/main/java/com/dickli/controller/WelcomeController.java
+++ docker-consumer/src/main/java/com/dickli/controller/WelcomeController.java
@@ -3,15 +3,18 @@ package com.dickli.controller;
 import com.dickli.service.WelcomeService;
 import org.apache.dubbo.common.utils.StringUtils;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController("welcome")
+@RequestMapping("/welcome")
+@Controller
 public class WelcomeController {
     @DubboReference
     private WelcomeService welcomeService;
 
-    @RequestMapping("")
+    @RequestMapping("/welcome")
+    @ResponseBody
     public String welcome(String userName) {
         if(StringUtils.isBlank(userName)){
             userName = "stranger";
--- docker-provider/docker-provider-service/src/main/java/com/dickli/service/impl/WelcomeServiceImpl.java
+++ docker-provider/docker-provider-service/src/main/java/com/dickli/service/impl/WelcomeServiceImpl.java
@@ -2,15 +2,18 @@ package com.dickli.service.impl;
 
 import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.dickli.service.WelcomeService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
 
 @DubboService
+@Slf4j
 public class WelcomeServiceImpl implements WelcomeService {
     @NacosValue(value = "${welcome:welcome}", autoRefreshed = true)
     private String welcome;
 
     @Override
     public String welcome(String userName) {
+        log.info("welcome:{},userName:{}",welcome,userName);
         return welcome + " " + userName;
     }
 }
